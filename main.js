880673476d28c4d86497757351b0e050c4d3e354
Olena Shatalova <08082023m@gmail.com>
2024-11-30 17:06:16 +0200

    add active link

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const openModalBtn = document.querySelector(".js-open-btn");
 const closeMenuBtn = document.querySelector(".js-close-menu-btn");
 const modalMenu = document.querySelector(".js-modal-menu");
-const links = document.querySelectorAll(".js-modal-menu a");
 const openOrderBtn = document.querySelector(".js-order-btn");
 const closeOrderBtn = document.querySelector(".js-close-order-btn");
 const orderOverlay = document.querySelector(".js-modal-overlay");
+const linksModal = document.querySelectorAll(".js-modal-menu a");
 
 const open = (x) => {
   x.classList.add("is-open");
@@ -14,13 +14,12 @@ const open = (x) => {
 const close = (x) => {
   x.classList.remove("is-open");
   document.body.style.overflow = "";
-  console.log("jjj");
 };
 
 openModalBtn.addEventListener("click", () => open(modalMenu));
 closeMenuBtn.addEventListener("click", () => close(modalMenu));
 
-links.forEach((link) => {
+linksModal.forEach((link) => {
   link.addEventListener("click", () => close(modalMenu));
 });
 
@@ -37,4 +36,35 @@ document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
     close(orderOverlay);
   }
+});
+
+const anchorSections = document.querySelectorAll(".js-anchor");
+const headerLinks = document.querySelectorAll(".js-header-link");
+
+const observerOptions = {
+  root: null, // Вікно перегляду
+  rootMargin: "0px", // Без додаткового відступу
+  threshold: 0.2, // Елемент повинен бути видимий на ..%
+};
+
+const observerCallback = (entries) => {
+  entries.forEach((entry) => {
+    const id = entry.target.id;
+
+    const link = document.querySelector(`a[href="#${id}"]`);
+
+    if (entry.isIntersecting) {
+      headerLinks.forEach((link) => link.classList.remove("active-link"));
+
+      if (link) {
+        link.classList.add("active-link");
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+anchorSections.forEach((anchor) => {
+  observer.observe(anchor);
 });
